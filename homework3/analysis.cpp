bc680f3a0d1884589057a057b8928db2f1ced696
dima-prezhentsov <70948858+dima-prezhentsov@users.noreply.github.com>
2021-05-07 12:47:31 +0300

    Add files via upload

--- homework3/analysis.cpp
+++ homework3/analysis.cpp
@@ -65,56 +65,29 @@ double TransportStation::getY() const {
 }
 
 void MaxStopsRoute(vector<TransportStation>& allStations) {
-    map<string, int> maxStationInRoute_BUS;
-    map<string, int> maxStationInRoute_TRAM;
-    map<string, int> maxStationInRoute_TROLLEYBUS;
+    map<string, map<string, int>> numberOfStopsOnRoute; // map<typeOfVehicle, map<routeNumber, value>>
+    map<string, pair<string, int>> result;     // map<typeOfVehicle, pair<resRouteNumber, maxValue>>
+
+
     for (auto station : allStations) {
-        if (station.getTypeOfVehicle() == "Трамвай") {
-            for (const auto& route : station.getRoutes()) {
-                maxStationInRoute_TRAM[route] += 1;
-            }
+        for (const auto& routeNumber : station.getRoutes()) {
+            numberOfStopsOnRoute[station.getTypeOfVehicle()][routeNumber] += 1;
         }
-        if (station.getTypeOfVehicle() == "Троллейбус") {
-            for (const auto& route : station.getRoutes()) {
-                maxStationInRoute_TROLLEYBUS[route] += 1;
-            }
-        }
-        if (station.getTypeOfVehicle() == "Автобус") {
-            for (const auto& route : station.getRoutes()) {
-                maxStationInRoute_BUS[route] += 1;
+    }
+
+    for (auto [typeOfVehicle, Route] : numberOfStopsOnRoute) {
+        for (auto [currentRouteNumber, currentValue] : Route) {
+            auto [resRouteNumber, maxValue] = result[typeOfVehicle];
+            if (currentValue > maxValue) {
+                result[typeOfVehicle] = {currentRouteNumber, currentValue};
             }
         }
     }
 
-    int maxRoute_TRAM = 0;
-    int maxRoute_BUS = 0;
-    int maxRoute_TROLLEYBUS = 0;
-    string Route_TRAM;
-    string Route_BUS;
-    string Route_TROLLEYBUS;
-
-    for (auto& el : maxStationInRoute_TRAM) {
-        if (el.second > maxRoute_TRAM) {
-            maxRoute_TRAM = el.second;
-            Route_TRAM = el.first;
-        }
+    for (auto [typeOfVehicle, resPair] : result) {
+        auto [resRouteNumber, resValue] = result[typeOfVehicle];
+        cout << "Route with the most " << typeOfVehicle << " stops is " << resRouteNumber << endl;
     }
-    for (auto& el : maxStationInRoute_BUS) {
-        if (el.second > maxRoute_BUS) {
-            maxRoute_BUS = el.second;
-            Route_BUS = el.first;
-        }
-    }
-    for (auto& el : maxStationInRoute_TROLLEYBUS) {
-        if (el.second > maxRoute_TROLLEYBUS) {
-            maxRoute_TROLLEYBUS = el.second;
-            Route_TROLLEYBUS = el.first;
-        }
-    }
-
-    cout << "Route with the most tram stops is " << Route_TRAM << endl;
-    cout << "Route with the most trolleybus stops is " << Route_TROLLEYBUS << endl;
-    cout << "Route with the most bus stops is " << Route_BUS << endl;
 
 }
 
@@ -134,78 +107,44 @@ void MaxStopsStreet(vector<TransportStation>& allStation) {
     }
     cout << "Street with the most stops is " << result << endl;
 }
-//todo all type of vehicle
+
 void LongestRoute(vector<TransportStation>& allStations) {
-    map<string, vector<TransportStation>> Routes_BUS;
-    map<string, vector<TransportStation>> Routes_TRAM;
-    map<string, vector<TransportStation>> Routes_TROLLEYBUS;
-    map<string, double> distance_BUS;
-    map<string, double> distance_TRAM;
-    map<string, double> distance_TROLLEYBUS;
-    double max_dist_BUS = 0;
-    double max_dist_TRAM = 0;
-    double max_dist_TROLLEYBUS = 0;
-    string result_BUS;
-    string result_TRAM;
-    string result_TROLLEYBUS;
-    //todo copy-paste
+
+
+    //fixed copy-paste
+    //fixed use unpacking for pairs
+    map<string, map<string, vector<TransportStation>>> Routes; // map<typeOfVehicle, map<routeNumber, stations vector>>
+    map<string, map<string, double>> distance;                 // map<typeOfVehicle, map<routeNumber, value>>
+    map<string, pair<double, string>> result;                  // map<typeOfVehicle, pair<maxValue, resultRouteNumber>>
+    string typeOfVehicle;
     for (auto& station : allStations) {
         if (!station.getStreet().empty()) {
-            if (station.getTypeOfVehicle() == "Автобус") {
-                for (const auto& rt : station.getRoutes()) {
-                    Routes_BUS[rt].push_back(station);
-                }
+            typeOfVehicle = station.getTypeOfVehicle();
+            for (const auto& routeNumber : station.getRoutes()) {
+                Routes[typeOfVehicle][routeNumber].push_back(station);
             }
-            if (station.getTypeOfVehicle() == "Трамвай") {
-                for (const auto& rt : station.getRoutes()) {
-                    Routes_TRAM[rt].push_back(station);
-                }
-            }
-            if (station.getTypeOfVehicle() == "Троллейбус") {
-                for (const auto& rt : station.getRoutes()) {
-                    Routes_TROLLEYBUS[rt].push_back(station);
-                }
-            }
-
-        }
-    }
-    //todo use unpacking for pairs
-    for (const auto& rt : Routes_BUS) {
-        for (size_t i = 0; i < rt.second.size() - 1; ++i) {
-            distance_BUS[rt.first] += sqrt(pow(rt.second[i + 1].getX() - rt.second[i].getX(), 2.0) + pow(rt.second[i + 1].getY() - rt.second[i].getY(), 2.0));
-        }
-    }
-    for (const auto& rt : Routes_TRAM) {
-        for (size_t i = 0; i < rt.second.size() - 1; ++i) {
-            distance_TRAM[rt.first] += sqrt(pow(rt.second[i + 1].getX() - rt.second[i].getX(), 2.0) + pow(rt.second[i + 1].getY() - rt.second[i].getY(), 2.0));
-        }
-    }
-    for (const auto& rt : Routes_TROLLEYBUS) {
-        for (size_t i = 0; i < rt.second.size() - 1; ++i) {
-            distance_TROLLEYBUS[rt.first] += sqrt(pow(rt.second[i + 1].getX() - rt.second[i].getX(), 2.0) + pow(rt.second[i + 1].getY() - rt.second[i].getY(), 2.0));
         }
     }
 
-    for (const auto& el : distance_BUS) {
-        if (el.second > max_dist_BUS) {
-            result_BUS = el.first;
-            max_dist_BUS = el.second;
-        }
-    }
-    for (const auto& el : distance_TRAM) {
-        if (el.second > max_dist_TRAM) {
-            result_TRAM = el.first;
-            max_dist_TRAM = el.second;
-        }
-    }
-    for (const auto& el : distance_TROLLEYBUS) {
-        if (el.second > max_dist_TROLLEYBUS) {
-            result_TROLLEYBUS = el.first;
-            max_dist_TROLLEYBUS = el.second;
+    for (auto [typeOfVehicle, stationsInRoute] : Routes) {
+        for (auto [routeNumber, stations] : stationsInRoute) {
+            for (size_t i = 0; i < stations.size() - 1; ++i) {
+                distance[typeOfVehicle][routeNumber] += sqrt(pow(stations[i + 1].getX() - stations[i].getX(), 2.0) + pow(stations[i + 1].getY() - stations[i].getY(), 2.0));
+            }
         }
     }
 
-    cout << "Bus route with max distance is " << result_BUS << endl;
-    cout << "Tram route with max distance is " << result_TRAM << endl;
-    cout << "Trolleybus route with max distance is " << result_TROLLEYBUS << endl;
+    for (auto [typeOfVehicle, distanceInRoute] : distance) {
+        for (auto [currentRouteNumber, currentValue] : distanceInRoute) {
+            auto [maxValue, resultRouteNumber] = result[typeOfVehicle];
+            if (currentValue > maxValue) {
+                result[typeOfVehicle] = {currentValue, currentRouteNumber};
+            }
+        }
+    }
+
+    for (auto [typeOfVehicle, resPair] : result) {
+        auto [value, routeNumber] = resPair;
+        cout << typeOfVehicle << " route with max distance is " << routeNumber << endl;
+    }
 }
